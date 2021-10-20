70cfba521afac0bdc3939f8472ff6c5688b290c5
Utku Sarioglu <utkusarioglu@hotmail.com>
2021-10-20 17:51:15 +0000

    Update __global.ts
    * The previous definitions in the module assumed that the env values
    would be available every time. This very faulty assumption has been
    replaced with one that makes more sense.
    
    * Env file is purposed to hold the local and testnet account information with
    the syntax:
     <net name>_<account name>=<address>:<pk>
     Some variables have been defined in ProcessEnv in preparation for this.

--- __global.ts
+++ __global.ts
@@ -1,9 +1,13 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
-      INFURA_API_KEY: string;
-      GOERLI_DEPLOYER_PK: string;
-      GETH_DEPLOYER_PK: string;
+      INFURA_API_KEY?: string;
+      COINMARKETCAP_API_KEY?: string;
+      LOCAL_DEPLOYER?: string;
+      GOERLI_DEPLOYER?: string;
+      LOCAL_USER1?: string;
+      LOCAL_USER2?: string;
+      LOCAL_USER3?: string;
     }
   }
 }
